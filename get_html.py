c15bc009506169c93d871c0775fceee12f310d92
Liam-Cafearo <lcafearo@gmail.com>
2018-05-29 08:02:34 +0100

    Added code to select only the title

--- get_html.py
+++ get_html.py
@@ -8,5 +8,26 @@ url = "https://en.wikipedia.org/wiki/Web_scraping"
 html_page = urlopen(url)
 html_text = html_page.read()
 
-# print html_test string
-print html_text
+# Set the range that we want filter on.
+# In this instance everything between the <title></title> tags
+start_tag = "<title>"
+end_tag = "</title>"
+
+"""
+Search for the tags.
+
+The html_text.find() function will give us the index of the very
+first '<' character that matches the string that we pass through.
+We want to search for everything from the end of the tag -
+i.e. ">This text"
+To achieve this we'll add the length of the 'start_tag' to the index of '<'
+charcter.
+
+This won't be necessary for the end tag. We just need the index for the '<'
+character.
+"""
+start_index = html_text.find(start_tag) + len(start_tag)
+end_index = html_text.find(end_tag)
+
+# Print everything between the two indexes in html_text
+print html_text[start_index:end_index]
