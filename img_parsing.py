d14edac660a8ccab6f67465abbeb95ce596bc99e
Liam-Cafearo <lcafearo@gmail.com>
2018-05-31 15:21:34 +0100

    Ammended comments

--- img_parsing.py
+++ img_parsing.py
@@ -6,8 +6,8 @@ html_page = urlopen(my_address)
 html_text = html_page.read()
 soup = BeautifulSoup(html_text, "html.parser")
 
-# Put all image elements in a list, because 'img' occurs more than
-# once when using 'find_all' it stores the results in a tuple.
+# Put all image elements in a list, each 'img tag gets stored
+# as a dictionary.
 images = soup.find_all("img")
 
 # Iterate over 'images' list and
